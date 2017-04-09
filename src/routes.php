a0efe2bf177c4b825d73cc374ebe805be6a79d3c
Lorenzo Salamante <enzo@webcraftph.com>
2017-04-09 22:41:50 +0800

    refactor routes into group

--- src/routes.php
+++ src/routes.php
@@ -9,63 +9,112 @@ $app->get('/', function ($request, $response, $args) {
   ]);
 });
 
-$app->get('/tarots', function ($request, $response, $args) {
-  $obj = new \App\Crud($this->db);
-  $data = $obj->all();
-  $response->getBody()->write(var_dump($data));
-  return $response;
+
+$app->group('/tarots', function () {
+
+  $this->get('', function($request, $response, $args){
+    $obj = new \App\Crud($this->db);
+    $data = $obj->all();
+
+    return $response->withJson($data, 200);
+  });
+
+  $this->get('/{id:[0-9]+}', function($request, $response, $args){
+    $obj = new \App\Crud($this->db);
+    $data = $obj->get($args['id']);
+
+    if(count($data) > 0){
+      return $response->withJson($data, 200);
+    }else{
+      return $response->withJson(array('message' => 'Not found'), 404);
+    }
+
+  });
+
+  $this->post('', function ($request, $response, $args) {
+    $data = $request->getParsedBody();
+
+    $obj = new \App\Crud($this->db);
+
+    try{
+      $last_insert_id = $obj->add($data);
+
+      $data = $obj->get($last_insert_id);
+
+      $response = $response->withAddedHeader(
+        'Location', $this['settings']['base_url'] . 'tarots/' . $last_insert_id
+      );
+      return $response->withJson($data, 201);
+
+    }
+    catch(Exception $e){
+
+      return $response->withJson(array('message' => 'Malformed syntax'), 400);
+
+    }
+
+  });
+
+  $this->delete('/{id:[0-9]+}', function($request, $response, $args){
+    $obj = new \App\Crud($this->db);
+    $result = $obj->delete($args['id']);
+
+    /**
+    * if more than 1 row(s) got deleted
+    */
+    if($result > 0){
+
+      return $response->withStatus(204);
+
+    }else{
+
+      return $response->withJson(array('message' => 'Not found'), 404);
+
+    }
+
+  });
+
+  $this->map(['PATCH', 'POST'], '/{id:[0-9]+}', function($request, $response, $args){
+    $data = $request->getParsedBody();
+
+    $obj = new \App\Crud($this->db);
+
+    $result = $obj->update($args['id'], $data);
+
+    if($result >= 0){
+
+      $data = $obj->get($args['id']);
+      return $response->withJson($data, 200);
+
+    }else{
+
+      return $response->withJson(array('message' => 'Not found'), 404);
+
+    }
+  });
+
 });
 
-$app->get('/tarot/{id}', function ($request, $response, $args) {
-  $obj = new \App\Crud($this->db);
-  $data = $obj->get($args['id']);
-  $response->getBody()->write(var_dump($data));
-  return $response;
-});
-
-$app->patch('/tarot/{id}', function ($request, $response, $args) {
-  $data = $request->getParsedBody();
-
-  $obj = new \App\Crud($this->db);
-  $result = $obj->update($args['id'], $data);
-  return $response->getBody()->write(var_dump($result));
-});
-
-$app->delete('/tarot/{id}', function ($request, $response, $args) {
-  $id = filter_var($args['id'], FILTER_SANITIZE_STRING);
-  $obj = new \App\Crud($this->db);
-  $result = $obj->delete($args['id']);
-  return $response->getBody()->write(var_dump($result));
-});
-
-$app->post('/tarot/new', function ($request, $response, $args) {
-  $data = $request->getParsedBody();
-
-  $obj = new \App\Crud($this->db);
-  $result = $obj->add($data);
-
-  $response->getBody()->write(var_dump($data));
-  return $response;
-});
-
-$app->post('/testUpload', function ($request, $response, $args){
-  $files = $request->getUploadedFiles();
-  $data = $request->getParsedBody();
-
-  $obj = new \App\Crud($this->db);
-
-  /**
-   * merge array of uploaded files + request body array
-   * @var array
-   */
-  $data = array_merge($obj->upload($files), $data);
-
-  $result = $obj->add($data);
-
-  return $response->getBody()->write(var_dump($result));
 
 
-});
+// $app->post('/testUpload', function ($request, $response, $args){
+//   $files = $request->getUploadedFiles();
+//   $data = $request->getParsedBody();
+//
+//   $obj = new \App\Crud($this->db);
+//
+//   /**
+//    * merge array of uploaded files + request body array
+//    * @var array
+//    */
+//   $data = array_merge($obj->upload($files), $data);
+//
+//   $result = $obj->add($data);
+//
+//   return $response->getBody()->write(var_dump($result));
+//
+//
+// });
 
 
 // $app->get('/[{name}]', function ($request, $response, $args) {
